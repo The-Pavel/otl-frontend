f8b8307f1db7ac2c6fdf042f59cf01489eda1413
Pavel Liser <liser.pavel@gmail.com>
2022-01-11 18:56:41 +0800

    fixed extra / in url

--- app/pages/_3_csv_prediction.py
+++ app/pages/_3_csv_prediction.py
@@ -16,8 +16,9 @@ def app():
         dataframe = pd.read_csv(uploaded_file)
         st.write(dataframe)
         json = dataframe.to_json()
-        url_post = 'https://otl-backend.herokuapp.com//uploadfile/'
+        url_post = 'https://otl-backend.herokuapp.com/uploadfile/'
         file = {"file": json}
         response = requests.post(url_post,files=file).json()
+        st.write(response)
         response_df = pd.read_json(response)
         st.write(response_df)
